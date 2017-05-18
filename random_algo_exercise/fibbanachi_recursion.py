dc441236d4f06f88ba5fb048092a5e7d02b339cc
amarinas <ali.marinas@icloud.com>
2017-05-18 14:53:46 -0700

    algo practice

---
+++ random_algo_exercise/fibbanachi_recursion.py
@@ -0,0 +1,20 @@
+# fibbanachi recursion
+# base case:
+# if fib 0, return 0
+# if fib 1, return 1
+# fib(2) = fib(0) +fib(1) = 1
+# fib(3) = fib(1) + fib(2) = 2
+# [0,1,1,2,3,5,8,13......]
+
+def fib(n):
+    print "the current", n
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+
+
+print(fib(3))
