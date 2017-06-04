bc2a5a47ce1004b12e7867ff5076e61b29b6a87d
amarinas <ali.marinas@icloud.com>
2017-06-04 11:03:01 -0700

    loop algo hackerrank day 5

---
+++ random_algo_exercise/loops.py
@@ -0,0 +1,9 @@
+# Given an integer,n , print its first 10 multiples. Each multiple n x i (where 1<=i <=10) should be printed on a new line in the form: n x i = result.
+
+import sys
+
+
+
+n = int(raw_input().strip())
+for i in range(1, 11):
+    print n, 'x', + i, '=', n * i
