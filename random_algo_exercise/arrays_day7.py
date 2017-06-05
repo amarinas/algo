1f0cc996affadb07ed4150d2926cb94c3b47cdfb
amarinas <ali.marinas@icloud.com>
2017-06-05 13:52:07 -0700

    day 7 arrays

---
+++ random_algo_exercise/arrays_day7.py
@@ -0,0 +1,10 @@
+# Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+from __future__ import print_function
+import sys
+
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+
+for num in arr:
+    print(num + " ", end='')
