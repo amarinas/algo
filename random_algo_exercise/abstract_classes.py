7e2baddf07323bcf2f55d1409ef9098406f107fc
amarinas <ali.marinas@icloud.com>
2017-06-14 09:04:22 -0700

    abstract_classes completed hacker rank

---
+++ random_algo_exercise/abstract_classes.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+class Book:
+    __metaclass__ = ABCMeta
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+    @abstractmethod
+    def display(): pass
+
+
+
+class MyBook(Book):
+    def __init__(self, title, author, price):
+        super(Book, self).__init__()
+
+    def display(self):
+        print 'Title: ' + title
+        print 'Author: ' + author
+        print 'Price: ' + str(price)
+
+
+title=raw_input()
+author=raw_input()
+price=int(raw_input())
+new_novel=MyBook(title,author,price)
+new_novel.display()
