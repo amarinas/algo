7b54f54e7521cefdbb9d7a4b69cff3b9007a043a
amarinas <ali.marinas@icloud.com>
2017-08-30 12:24:04 -0700

    practice algo

---
+++ JavascriptPractice/dontworrybehappy.js
@@ -0,0 +1,12 @@
+//Create beCheerful() within it console.log string "good morning!" call it 98 times
+
+//create function for Print
+function beCheerful() {
+  console.log("good morning!");
+
+}
+
+//Calling the function 98 times
+for (var i = 1; i < 98; i++) {
+  beCheerful();
+}
---
+++ JavascriptPractice/multiplesofsix.js
@@ -0,0 +1,7 @@
+//print multiples of 6 up to 60,000 using While
+
+var i = 6;
+while (i <= 60000){
+  console.log(i);
+  i += 6
+}
