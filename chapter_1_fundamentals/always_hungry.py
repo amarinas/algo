45479867bd876a82609d3cebf78ec443137f221c
ali <ali.marinas@icloud.com>
2020-12-28 22:30:48 -0800

    comprehensions

--- chapter_1_fundamentals/always_hungry.py
+++ chapter_1_fundamentals/always_hungry.py
@@ -2,19 +2,24 @@
 # print yummy
 # if there is no food then then print im hungry
 
-def always_hungry(arr):
-    status = True
-    for i in range(len(arr)):
-        if arr[i] == "food":
-            print "yummy"
-            status = False
+# def always_hungry(arr):
+#     status = True
+#     for i in range(len(arr)):
+#         if arr[i] == "food":
+#             print "yummy"
+#             status = False
+#
+#     if status == True:
+#         print "im hungry"
+#
+#
+#
+#
+#
+# always_hungry(["tomatoe", "lop", 3, "sk" ,5])
+# always_hungry(["tomatoe", "food", 3, "food" ,5])
 
-    if status == True:
-        print "im hungry"
+arr =["tomatoe", "food", 3, "food" ,5]
+diet = [True if arr[i] == "food"  else False for i in range(len(arr))]
 
-
-
-
-
-always_hungry(["tomatoe", "lop", 3, "sk" ,5])
-always_hungry(["tomatoe", "food", 3, "food" ,5])
+print diet
