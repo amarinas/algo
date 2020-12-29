f6a61da47e9b917e1e80b7550ad61b3584e0e8a3
ali <ali.marinas@icloud.com>
2020-12-28 18:25:01 -0800

    practice

---
+++ chapter2_fundamentals_II/palindrome.py
@@ -0,0 +1,11 @@
+def palindrome(num):
+    rev = num[::-1]
+    if rev == num:
+        print ("palindrome")
+    else:
+        print("not palindrome")
+
+
+def __main__(self):
+
+    print(palindrome("madam"))
