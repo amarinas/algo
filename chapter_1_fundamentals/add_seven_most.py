124933a3e9ad1891132dc370be3e34053aa42cc2
ali <ali.marinas@icloud.com>
2020-12-28 18:40:48 -0800

    comprehensions

--- chapter_1_fundamentals/add_seven_most.py
+++ chapter_1_fundamentals/add_seven_most.py
@@ -2,10 +2,15 @@
 # retrun a new array will all values exept first
 # adding 7 to each
 # do not alter
-def add_seven_most(arr):
-    newarr = []
-    for i in range(1,len(arr)):
-        newarr.append(arr[i]+ 7)
-    return newarr
+# def add_seven_most(arr):
+#     newarr = []
+#     for i in range(1,len(arr)):
+#         newarr.append(arr[i]+ 7)
+#     return newarr
+num = [3,44,5,6,4,3,4,33]
+newArr = [num[i] + 7 for i in range(1,len(num))]
 
-print(add_seven_most([3,44,5,6,4,3,4,33]))
+# print(add_seven_most([3,44,5,6,4,3,4,33]))
+
+
+print newArr
